c52de6070246ed882baf9724b1a5b8ed6de00be4
FrancescoFolgheraiter <f.folgheraiter@hotmail.it>
2023-12-12 14:57:33 +0100

    create layout page

--- pari e dispari/js/scripts.js
+++ pari e dispari/js/scripts.js
@@ -1,1 +1,21 @@
-alert("funziona")
+/*
+CONSEGNA
+Pari e Dispari
+L'utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+
+SVOLGIMENTO
+1)prendo in input se l'utento ha scelto pari o dispari
+2)prendo in input un numero dall'utemte da 1 a 5
+3)genero numero random per il pc
+4)sommo i due numeri
+5)creo la funzione che controlla se la somma è pari o dispari
+    -prendo come argomento la somma
+    -uso operatore modulo per verificare se è pari o dispari
+    -restituisco 0 per pari
+    -restituisco 1 per dispari;
+6) dichiaro chi ha vinto
+*/
